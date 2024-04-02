56dffd6bfd1098843cc817f5d002ab78994e1fcc
Mathis <lb.brangeon@gmail.com>
2024-04-02 13:59:09 +0200

    💄Add variables style

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/base/_variable.scss";`,
+      },
+    },
+  },
   resolve: {
     alias: {
       src: "/src",
+      "@": "/src",
     },
   },
 });
